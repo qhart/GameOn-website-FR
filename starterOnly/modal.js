83483f0e66b117014f10afca27f088988779c44c
qhart <quentinhart51@gmail.com>
2022-11-10 17:46:37 +0100

    add disable submit function

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,16 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const modalClose = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
+const submitBtn = document.querySelector(".btn-submit");
+
+// Form data
+
+const firstName = document.getElementById('first').value;
+
+// Regex rules
+
+const firstNameRegex = '[a-z][A-Z]{2,}';
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -27,5 +37,18 @@ function launchModal() {
 // close modal form
 function closeModal() {
   modalbg.style.display = "none";
+}
+
+// Disable submit function
+function disableSubmit(disabled) {
+  if (disabled) {
+    document
+      .getElementByClass("btn-submit")
+      .setAttribute("disabled", true);
+  } else {
+    document
+      .getElementByClass("btn-submit")
+      .removeAttribute("disabled");
+  }
 }
 
